56eef9cae1428c86582979c15e2026132e2da238
Frank Albenesius <frankalbenesius@gmail.com>
2020-02-13 12:33:35 -0600

    Make it frank, tho.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello. I am Frank.
+      <div className="Links">
+        <a href="https://www.github.com/frankalbenesius/">github</a>
+        <a href="https://www.instagram.com/frankalbenesius/">instagram</a>
+        <a href="https://www.twitter.com/frankalbenesius/">twitter</a>
+        <a href="mailto:frankalbenesius@gmail.com">email</a>
+      </div>
     </div>
   );
 }
